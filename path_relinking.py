2cb7f0c00edb098bfb0c633a099f3be99b734361
ensabai <ensabai@alumni.uv.es>
2025-05-15 21:39:54 +0200

    Selección de archivos necesarios

--- path_relinking.py
+++ path_relinking.py
@@ -1,95 +1,63 @@
-from structure import instance, solution
-from algorithms import grasp
-from copy import deepcopy
-import pandas as pd
-import time
+import copy
+import random
 
-def relink(start, guide, start_time, max_time):
-    current = deepcopy(start)
-    best = deepcopy(start)
-    to_add = guide["sol"] - current["sol"]
-    to_remove = current["sol"] - guide["sol"]
+def calculate_sum_dist_to_sol(instance, sol_set, n):
+    sum_dist = [0.0] * n
+    for i in range(n):
+        for s in sol_set:
+            sum_dist[i] += instance['d'][i][s]
+        sum_dist[i] = round(sum_dist[i], 2)
+    return sum_dist
+
+def path_relinking(sol_start, sol_guide, beta):
+    instance = sol_start['instance']
+    n = instance['n']
+
+    to_remove = list(sol_start['sol'] - sol_guide['sol'])
+    to_add = list(sol_guide['sol'] - sol_start['sol'])
+
+    current = copy.deepcopy(sol_start)
+    best = copy.deepcopy(current)
+
+    sum_dist = calculate_sum_dist_to_sol(instance, current['sol'], n)
 
     while to_add:
-        vecinos = []
-        for a in to_add:
-            for r in to_remove:
-                # Control de tiempo dentro de relink
-                if time.time() - start_time > max_time:
-                    print("Tiempo límite alcanzado dentro de relink")
-                    return best
-                
-                nuevo = deepcopy(current)
-                nuevo["sol"].remove(r)
-                nuevo["sol"].add(a)
-                nuevo["of"] = solution.evaluate(nuevo)
-                vecinos.append(nuevo)
-        if not vecinos:
+        add_vals = [(u, sum_dist[u]) for u in to_add]
+        rem_vals = [(v, sum_dist[v]) for v in to_remove]
+
+        if not add_vals or not rem_vals:
             break
 
-        current = max(vecinos, key=lambda s: s["of"])
-        if current["of"] > best["of"]:
-            best = deepcopy(current)
-            
-        to_add = guide["sol"] - current["sol"]
-        to_remove = current["sol"] - guide["sol"]
+        min_a, max_a = min(add_vals, key=lambda x: x[1])[1], max(add_vals, key=lambda x: x[1])[1]
+        add_thresh = max_a - beta * (max_a - min_a)
+        add_rcl = [u for u, val in add_vals if val >= add_thresh]
+
+        min_r, max_r = min(rem_vals, key=lambda x: x[1])[1], max(rem_vals, key=lambda x: x[1])[1]
+        rem_thresh = min_r + beta * (max_r - min_r)
+        rem_rcl = [v for v, val in rem_vals if val <= rem_thresh]
+
+        if not add_rcl or not rem_rcl:
+            break
+
+        u = random.choice(add_rcl)
+        v = random.choice(rem_rcl)
+
+        delta = (sum_dist[u] - instance['d'][u][v]) - sum_dist[v]
+        delta = round(delta, 2)
+
+        current['sol'].remove(v)
+        current['sol'].add(u)
+        current['of'] += delta
+        current['of'] = round(current['of'], 2)
+
+        for x in range(n):
+            sum_dist[x] = sum_dist[x] - instance['d'][x][v] + instance['d'][x][u]
+            sum_dist[x] = round(sum_dist[x], 2)
+
+        to_remove.remove(v)
+        to_add.remove(u)
+
+        if current['of'] > best['of']:
+            best = copy.deepcopy(current)
 
     return best
-
-def pathRelinking(path, alpha, tiempo, elite_size):
-    """
-    Ejecuta GRASP + Path Relinking sobre una instancia.
-
-    Parámetros:
-        path: ruta del archivo de instancia.
-        alpha: valor alfa para GRASP.
-        tiempo: tiempo total (se divide entre GRASP y PR).
-        elite_size: número de soluciones élite para PR.
-
-    Devuelve:
-        mejor_pr: mejor solución encontrada por PR.
-        df_pr: DataFrame con todas las soluciones PR.
-    """
-    # Leer la instancia
-    inst = instance.readInstance(path)
-
-    # Ejecutar GRASP con la mitad del tiempo asignado
-    _, soluciones, _ = grasp.execute(inst, tiempo / 2, alpha)
-
-    # Ordenar soluciones y construir conjunto élite
-    soluciones.sort(key=lambda s: -s['of'])  # orden descendente
-    elite = []
-    seen = set()
-    for sol in soluciones:
-        frozen = frozenset(sol['sol'])
-        if frozen not in seen:
-            elite.append(sol)
-            seen.add(frozen)
-        if len(elite) >= elite_size:
-            break
-    
-    # Iniciamos control del tiempo para la parte de PR
-    start_time = time.time()
-    # Aplicamos PR entre pares de soluciones élite
-    soluciones_pr = []
-    mejor_pr = None
-
-    for i in range(len(elite)):
-        for j in range(i + 1, len(elite)):
-            # Verifica si se excedió el tiempo total asignado
-            if time.time() - start_time > tiempo:
-                print("Límite de tiempo alcanzado en PR; saliendo de los bucles.")
-                break
-
-            pr1 = relink(elite[i], elite[j], start_time, tiempo)
-            pr2 = relink(elite[j], elite[i], start_time, tiempo)
-
-            soluciones_pr.extend([pr1, pr2])
-            
-            for sol in [pr1, pr2]:
-                if mejor_pr is None or sol["of"] > mejor_pr["of"]:
-                    mejor_pr = sol
-
-    # Nota: Si se sale anticipadamente, soluciones_pr contendrá solo aquellas generadas hasta ese momento.
-    df_pr = pd.DataFrame(soluciones_pr)
-    return mejor_pr, df_pr
--- path_relinking_todas_instancias.py
+++ path_relinking_todas_instancias.py
@@ -7,7 +7,7 @@ from structure.instance import readInstance
 from structure import solution
 from localsearch import lsbestimp
 from constructives import cgrasp
-
+from path_relinking import path_relinking
 
 def solution_difference(sol1, sol2):
     return len(sol1['sol'] ^ sol2['sol'])
@@ -65,68 +65,6 @@ def run_grasp(inst, tiempo, alpha, num_elite, diversity_weight):
 
     return best, all_unique_solutions, time.time() - time_start, elite_set
 
-
-def calculate_sum_dist_to_sol(instance, sol_set, n):
-    sum_dist = [0.0] * n
-    for i in range(n):
-        for s in sol_set:
-            sum_dist[i] += instance['d'][i][s]
-        sum_dist[i] = round(sum_dist[i], 2)
-    return sum_dist
-
-
-def path_relinking(sol_start, sol_guide, beta):
-    instance = sol_start['instance']
-    n = instance['n']
-
-    to_remove = list(sol_start['sol'] - sol_guide['sol'])
-    to_add = list(sol_guide['sol'] - sol_start['sol'])
-
-    current = copy.deepcopy(sol_start)
-    best = copy.deepcopy(current)
-
-    sum_dist = calculate_sum_dist_to_sol(instance, current['sol'], n)
-
-    while to_add:
-        add_vals = [(u, sum_dist[u]) for u in to_add]
-        rem_vals = [(v, sum_dist[v]) for v in to_remove]
-
-        if not add_vals or not rem_vals:
-            break
-
-        min_a, max_a = min(add_vals, key=lambda x: x[1])[1], max(add_vals, key=lambda x: x[1])[1]
-        add_thresh = max_a - beta * (max_a - min_a)
-        add_rcl = [u for u, val in add_vals if val >= add_thresh]
-
-        min_r, max_r = min(rem_vals, key=lambda x: x[1])[1], max(rem_vals, key=lambda x: x[1])[1]
-        rem_thresh = min_r + beta * (max_r - min_r)
-        rem_rcl = [v for v, val in rem_vals if val <= rem_thresh]
-
-        if not add_rcl or not rem_rcl:
-            break
-
-        u = random.choice(add_rcl)
-        v = random.choice(rem_rcl)
-
-        delta = (sum_dist[u] - instance['d'][u][v]) - sum_dist[v]
-        delta = round(delta, 2)
-
-        current['sol'].remove(v)
-        current['sol'].add(u)
-        current['of'] += delta
-        current['of'] = round(current['of'], 2)
-
-        for x in range(n):
-            sum_dist[x] = sum_dist[x] - instance['d'][x][v] + instance['d'][x][u]
-            sum_dist[x] = round(sum_dist[x], 2)
-
-        to_remove.remove(v)
-        to_add.remove(u)
-
-        if current['of'] > best['of']:
-            best = copy.deepcopy(current)
-
-    return best
 
 
 def ejecutar_grasp_pr(total_time_limit=60, carpeta="instances"):
@@ -191,4 +129,4 @@ def ejecutar_grasp_pr(total_time_limit=60, carpeta="instances"):
 
 if __name__ == "__main__":
     random.seed(1)
-    ejecutar_grasp_pr(total_time_limit=60)
+    ejecutar_grasp_pr(total_time_limit=0.5)
